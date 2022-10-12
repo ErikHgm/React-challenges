860abbd1841dabef19da7b7bba05b4e5970ea26d
Erik <erikhgm@gmail.com>
2022-10-12 21:27:07 +0000

    feat: create Sidebar function

---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,11 @@
+import React from "react";
+import css from "./css/Sidebar.module.css"
+
+function Sidebar(){
+    return <div className = {css.sidebar} ><a>MyPhotos</a>
+    <a>MyIllustrations</a>
+    <a>MyPaintings</a></div>
+    
+}
+
+export default Sidebar;
